6bdd8425dffcde34b73945a0426090798c4c873e
dejobratic <dejobratic@gmail.com>
2025-08-14 22:31:06 +0200

    UI enhancements;

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { Router, RouterOutlet, NavigationEnd } from '@angular/router';
+import { filter, map } from 'rxjs/operators';
+import { signal } from '@angular/core';
 
 import { AppHeader } from '@eristic/app/components/app-header/app-header';
 import { Sidebar } from '@eristic/app/components/sidebar/sidebar';
@@ -11,4 +13,17 @@ import { AppFooter } from '@eristic/app/components/app-footer/app-footer';
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {}
+export class App {
+  private router = inject(Router);
+  
+  isHomePage = signal(true);
+  
+  constructor() {
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd),
+      map(event => (event as NavigationEnd).url)
+    ).subscribe(url => {
+      this.isHomePage.set(url === '/');
+    });
+  }
+}
